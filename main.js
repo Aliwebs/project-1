448f8cd8139bb7b46adb77ca59e31cb4b168ac2a
Aliwebs <11ASkhankausar@gmail.com>
2021-04-16 13:27:56 +0100

    Improved grid, added proper food and eating

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 const grid = document.querySelector('#game-window')
-const width = 31 //change this to 30 later
+const width = 25
+const height = (width + 3)
 let gridArray = []
-let centerBox = [383, 577]
+let score = 0
 //* MVP 
 // grey comments are guesses on how I could go about it
+
+
+
+
 
 //? create a grid 
 // create a grid how Nick did in lesson needs to be a lot more bigger and 
 // the grid itself will be hidden*
-for (let i = 1; i <= width ** 2; i++) {
+for (let i = 1; i <= width ** 2 + (width * 3); i++) {
   const div = document.createElement('div')
-  div.setAttribute('id', i)
   div.style.width = `${100 / width}%`
-  div.style.height = `${100 / width}%`
+  div.id = i
+  div.style.height = `${100 / height}%`
   grid.appendChild(div)
   gridArray.push(div)
 }
+
 //? add blueprint onto grid, for MVP it will be a static board
 // Add the board on top of the grid, this could be coordinates of where 
 // all the walls will go or viceversa
-//480 is center
+
+
+// ? Code to create a static board
 // let staticGrid = []
 
 // grid.addEventListener('click', (e) => {
 //   if (e.target.localName === 'div') {
 //     if (!staticGrid.includes(e.target.innerHTML)) {
 //       staticGrid.push(e.target.innerHTML)
-//       e.target.classList.add('wall')
+//       e.target.classList.toggle('wall')
 //     } else {
 //       staticGrid = staticGrid.filter(num => num !== e.target.innerHTML)
-//       e.target.classList.remove('wall')
+//       e.target.classList.toggle('wall')
 //     }
 //   }
 // })
@@ -37,35 +45,64 @@ for (let i = 1; i <= width ** 2; i++) {
 // document.querySelector('#logmap').addEventListener('click', () => {
 //   staticGrid.forEach(cell => console.log(cell))
 // })
-
+// from the walls array gives the class wall to all walls
 map1.forEach(number => {
-  gridArray[number].classList.add('wall')
+  gridArray[number - 1].classList.add('wall')
 })
+// gives the class empty to all other boxes outside player path 
+map1Exclude.forEach(number => {
+  gridArray[number - 1].classList.add('empty')
+})
+
+
 
 
 //? place the food that pacman needs to collect
 //place food on all free path cells
 gridArray.forEach(cell => {
-  if (!cell.classList[0]) {
-    cell.classList.add('food')
+  if (cell.classList[0] !== 'wall' && cell.classList[0] !== 'empty') {
+    const span = document.createElement('span')
+    span.classList.add('food')
+    cell.appendChild(span)
+    cell.classList.add('path')
   }
 })
 
-// start the game - loop 
+// ? start the game - loop 
+let speed = 0
+// let x = 0
+// let y = 0
+setInterval(() => {
+  //! pacman stops moving if it hits a wall while moving in a 
+  //! given direction until player turns pacman
+  if (gridArray[pacman + speed].classList[0] === 'path') {
+    gridArray[pacman].classList.remove('pacman')
+    pacman = pacman + speed
+    //! pacman eats the food as it moves trough the maze
+    gridArray[pacman].classList.add('pacman')
+    if (gridArray[pacman].children[0].classList[0] === 'food') {
+      gridArray[pacman].children[0].classList.remove('food')
+      //keep tracked of food eaten
+      score++
+    }
 
+  }
+
+}, 300)
 //? spawn pacman in predifined location without any movement
 //span pacman under the ghost spawn box 
-let pacman = 728
+let pacman = 562
 gridArray[pacman].classList.add('pacman')
 
 
 //? spawn 4 ghosts in spawn box
 //spawn 4 ghosts in the ghost box and make them exit with a delay of 2 seconds
 // each. i.e. second won't leave until first is gone for 2 seconds
-const ghosts = [357, 449, 450, 451]
+const ghosts = [287, 336, 337, 338]
 
 ghosts.forEach(ghost => {
-  gridArray[ghost].classList.add('ghosts')
+  gridArray[ghost].classList.add('ghost')
+  gridArray[ghost].children[0].classList.remove('food')
 })
 
 //? have ghosts move towards random directions in the grid
@@ -75,15 +112,6 @@ ghosts.forEach(ghost => {
 
 //? listen to player input and turn packman accoridngly
 // listen to player input allow players to play with either W/A/S/D or arrow keys
-let speed = 0
-setInterval(() => {
-  if (gridArray[pacman + speed].classList[0] === 'food') {
-    gridArray[pacman].classList.remove('pacman')
-    pacman = pacman + speed
-    gridArray[pacman].classList.add('pacman')
-    console.log(speed)
-  }
-}, 500)
 
 document.addEventListener('keydown', (e) => {
   const key = e.key
@@ -119,12 +147,7 @@ document.addEventListener('keydown', (e) => {
 
 })
 
-//! pacman eats the food as it moves trough the maze
 
-//! pacman stops moving if it hits a wall while moving in a 
-//! given direction until player turns pacman
-
-//keep tracked of food eaten
 
 //! player loses if pacman hits a ghost
 
